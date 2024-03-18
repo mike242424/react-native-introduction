4e2217005f8c7d932788bded71a49de82ea5c6aa
mike242424 <mikebyers24@gmail.com>
2024-03-18 14:00:07 -0400

    creating first component

--- App.js
+++ App.js
@@ -1,3 +1,13 @@
-const App = () => {};
+import { Text, View } from 'react-native';
+import Profile from './components/Profile';
+
+const App = () => {
+  return (
+    <View style={{ alignItems: 'center', paddingTop: 100 }}>
+      <Text>Hello</Text>
+      <Profile />
+    </View>
+  );
+};
 
 export default App;
---
+++ components/Profile.jsx
@@ -0,0 +1,14 @@
+import { Text, TouchableOpacity } from 'react-native';
+
+const Profile = () => {
+  return (
+    <>
+      <Text>Profile</Text>
+      <TouchableOpacity>
+        <Text>Click</Text>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+export default Profile;
