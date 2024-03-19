048c732c189339b4808536b1c844a9aa4485ede2
mike242424 <mikebyers24@gmail.com>
2024-03-19 10:45:04 -0400

    more styles added

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 // import Profile from './components/Profile';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { style } from './App.style';
@@ -8,6 +8,9 @@ const App = () => {
     <SafeAreaProvider>
       <SafeAreaView>
         <Text style={style.title}>Hello</Text>
+        <View style={style.square}>
+          <Text style={style.innerText}>Hello</Text>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- App.style.js
+++ App.style.js
@@ -3,5 +3,16 @@ import { StyleSheet } from 'react-native';
 export const style = StyleSheet.create({
   title: {
     color: 'red',
+    alignSelf: 'center',
+  },
+  square: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'aqua',
+    alignSelf: 'center',
+  },
+  innerText: {
+    color: 'white',
+    alignSelf: 'center',
   },
 });
