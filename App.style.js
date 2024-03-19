9d4baa5bac06ef0fc6c5549c1572cd4ff58c08b8
mike242424 <mikebyers24@gmail.com>
2024-03-19 11:01:20 -0400

    justifyContent space-between

--- App.style.js
+++ App.style.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
   boxOne: { height: 100, width: 100, backgroundColor: 'red' },
   boxTwo: { height: 100, width: 100, backgroundColor: 'blue' },
