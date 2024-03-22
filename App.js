4ee5cb49e124f61f9300499ead9b4934e8a69fff
mike242424 <mikebyers24@gmail.com>
2024-03-22 11:38:19 -0400

    iOS vs Andriod

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import { Alert, Text, TouchableOpacity } from 'react-native';
+import { Alert, Platform, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { style } from './App.style';
-import ProfileCard from './components/ProfileCard/ProfileCard';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={style.container}>
-        <ProfileCard
+        {/* <ProfileCard
           name="Mike"
           message={
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptatibus at sunt.'
           }
-        />
+        /> */}
+        {Platform.OS === 'android' ? (
+          <Text>Hi on Android</Text>
+        ) : (
+          <Text>Hi on iOS</Text>
+        )}
       </SafeAreaView>
     </SafeAreaProvider>
   );
